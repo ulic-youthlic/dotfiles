dca207d3c4faf4f709d678c9d9bc68ca1e2d2e31
ulic-youthlic <ulic.youthlic@gmail.com>
2024-08-15 11:32:52 +0800

    add fnm as node version manager

---
+++ pwsh/completion-template/fnm.ps1
@@ -0,0 +1,7 @@
+$Script:Name = "fnm"
+$Script:File = Join-Path -Path $env:PowerShell_Completion_Dir -ChildPath "$Name.ps1"
+if ($null -eq (Get-Command $Name -ErrorAction SilentlyContinue)) {
+    Write-Host ([String]::Format("[Warn]: Unable to find {0} in your PATH", $Name))
+} else {
+    fnm completions --shell power-shell > $File
+}
