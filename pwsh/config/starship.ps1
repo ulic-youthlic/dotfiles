1b0ffc47e90106022ac608952e67d9f51426e2be
ulic-youthlic <ulic.youthlic@gmail.com>
2024-08-22 10:08:38 +0800

    update starship init configuration

--- pwsh/config/starship.ps1
+++ pwsh/config/starship.ps1
@@ -1,3 +1,3 @@
 # starship
 $ENV:STARSHIP_CONFIG = "$HOME\.config\starship\config.toml"
-Invoke-Expression (&starship init powershell)
+Invoke-Expression (& starship init powershell --print-full-init | Out-String)
